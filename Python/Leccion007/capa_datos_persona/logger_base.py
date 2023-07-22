090c2403e9ff39e2578e76830cab8803465055b0
piaolivera <piaoliveraa@gmail.com>
2023-07-22 14:56:40 -0300

    modificaciones

--- Python/Leccion007/capa_datos_persona/logger_base.py
+++ Python/Leccion007/capa_datos_persona/logger_base.py
@@ -10,9 +10,9 @@ log.basicConfig(level=log.DEBUG,
                 ])
 
 
-
-log.debug('Mensaje a nivel debug')
-log.info('Mensaje a nivel info')
-log.warning('Mensaje a nivel warning')
-log.error('Mensaje a nivel error')
-log.critical('Mensaje a nivel critical')
+if __name__== '__main__':
+    log.debug('Mensaje a nivel debug')
+    log.info('Mensaje a nivel info')
+    log.warning('Mensaje a nivel warning')
+    log.error('Mensaje a nivel error')
+    log.critical('Mensaje a nivel critical')
